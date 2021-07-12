f21ecb2e7091490ed25d6b3636893efd2eec9ac7
narin111 <jungnr11@naver.com>
2021-07-12 17:20:32 +0900

    midtest

--- json_parser.py
+++ json_parser.py
@@ -3,43 +3,63 @@ from urllib.request import urlopen
 from urllib.parse import urlencode, unquote, quote_plus
 import urllib.request
 
+
 with open('sidosgg.json', encoding='UTF8') as file:
     data = json.load(file)
 
 # 기본현황 
-basic = "https://e-childschoolinfo.moe.go.kr/api/notice/basicInfo.do?"
+basic = "https://e-childschoolinfo.moe.go.kr/api/notice/basicInfo.do"
 # 직위.자격별 교직원현황 
-teach = "https://e-childschoolinfo.moe.go.kr/api/notice/teachersInfo.do?"
+teach = "https://e-childschoolinfo.moe.go.kr/api/notice/teachersInfo.do"
 # 수업일수 현황 
-teach_days = "https://e-childschoolinfo.moe.go.kr/api/notice/lessonDay.do?"
+teach_days = "https://e-childschoolinfo.moe.go.kr/api/notice/lessonDay.do"
 # 급식운영현황 
-meal = "https://e-childschoolinfo.moe.go.kr/api/notice/schoolMeal.do?"
+meal = "https://e-childschoolinfo.moe.go.kr/api/notice/schoolMeal.do"
 # 통학차량현황 
-bus = "https://e-childschoolinfo.moe.go.kr/api/notice/schoolBus.do?"
+bus = "https://e-childschoolinfo.moe.go.kr/api/notice/schoolBus.do"
 # 근속연수 현황 
-work_year = "https://e-childschoolinfo.moe.go.kr/api/notice/yearOfWork.do?"
+work_year = "https://e-childschoolinfo.moe.go.kr/api/notice/yearOfWork.do"
 # 환경위생 관리현황 
-enviro = "https://e-childschoolinfo.moe.go.kr/api/notice/environmentHygiene.do?"
+enviro = "https://e-childschoolinfo.moe.go.kr/api/notice/environmentHygiene.do"
 # 안전점검, 교육 실시현황 
-safety = "https://e-childschoolinfo.moe.go.kr/api/notice/safetyEdu.do?"
+safety = "https://e-childschoolinfo.moe.go.kr/api/notice/safetyEdu.do"
 # 공제회 가입 현황 
-deduc = "https://e-childschoolinfo.moe.go.kr/api/notice/deductionSociety.do?"
+deduc = "https://e-childschoolinfo.moe.go.kr/api/notice/deductionSociety.do"
 # 보험별 가입 현황 
-insur = "https://e-childschoolinfo.moe.go.kr/api/notice/insurance.do?"
+insur = "https://e-childschoolinfo.moe.go.kr/api/notice/insurance.do"
 
 apikey = "2039b8c5db3c4385b39b00ae74b783cc"
 
-for one in data:
+for one in data: # sidosgg.json 추출
     sidoCode = one['시도코드']
     sggCode = one['시군구코드']
-    queryParams = '?' + urlencode({ quote_plus('key') : '2039b8c5db3c4385b39b00ae74b783cc', quote_plus('sidoCode') : sidoCode , quote_plus('sggCode') : sggCode })
+    
+    # queryParams = '?' + urlencode({ quote_plus('key') : '2039b8c5db3c4385b39b00ae74b783cc', quote_plus('sidoCode') : str(sidoCode) , quote_plus('sggCode') : str(sggCode) })
+    queryParams = '?' + "key=" + apikey + "&" + "sidoCode=" + str(sidoCode) + "&" + "sggCode=" + str(sggCode)
+    
     url = basic
 
-    # req = urllib.request.urlopen(url)
-    req = urllib.request.Request(url + unquote(queryParams))
-    res = req.realine()
-
+    
+    req = urllib.request.urlopen(url+queryParams)
+    res = req.readline()
+    
+    j = json.loads(res)
     print(sidoCode, sggCode)
+    for i in j:
+        print(j["kinderInfo"][i]["kindername"])
+ 
+    
+
+    # req = urllib.request.urlopen(url)
+    # req = urllib.request.Request(url + unquote(queryParams))
+    # result_data = req.json()
+    # print(result_data['kinderInfo']['kindername'])
+    # get_data = requests.get(url + unquote(queryParams))
+    # result_data = get_data.json()
+    # print(result_data)
+
+
+    # print(sidoCode, sggCode)
 
 
 
