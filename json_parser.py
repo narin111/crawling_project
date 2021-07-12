68157bfa2cebb5c5f8e31c08f7838fa838ff16f4
narin111 <jungnr11@naver.com>
2021-07-12 16:11:36 +0900

    parse

---
+++ json_parser.py
@@ -0,0 +1,43 @@
+import json
+from urllib.request import urlopen
+
+with open('sidosgg.json', encoding='UTF8') as file:
+    data = json.load(file)
+
+# 기본현황 
+basic = "https://e-childschoolinfo.moe.go.kr/api/notice/basicInfo.do?"
+# 직위.자격별 교직원현황 
+teach = "https://e-childschoolinfo.moe.go.kr/api/notice/teachersInfo.do?"
+# 수업일수 현황 
+teach_days = "https://e-childschoolinfo.moe.go.kr/api/notice/lessonDay.do?"
+# 급식운영현황 
+meal = "https://e-childschoolinfo.moe.go.kr/api/notice/schoolMeal.do?"
+# 통학차량현황 
+bus = "https://e-childschoolinfo.moe.go.kr/api/notice/schoolBus.do?"
+# 근속연수 현황 
+work_year = "https://e-childschoolinfo.moe.go.kr/api/notice/yearOfWork.do?"
+# 환경위생 관리현황 
+enviro = "https://e-childschoolinfo.moe.go.kr/api/notice/environmentHygiene.do?"
+# 안전점검, 교육 실시현황 
+safety = "https://e-childschoolinfo.moe.go.kr/api/notice/safetyEdu.do?"
+# 공제회 가입 현황 
+deduc = "https://e-childschoolinfo.moe.go.kr/api/notice/deductionSociety.do?"
+# 보험별 가입 현황 
+insur = "https://e-childschoolinfo.moe.go.kr/api/notice/insurance.do?"
+
+
+for one in data:
+    sidoCode = one['시도코드']
+    sggCode = one['시군구코드']
+    url = basic + str(sidoCode) + str(sggCode)
+    print(sidoCode, sggCode)
+
+
+
+#건물현황 https://e-childschoolinfo.moe.go.kr/api/notice/building.do
+#교실면적현황 https://e-childschoolinfo.moe.go.kr/api/notice/classArea.do
+
+apikey = "	2039b8c5db3c4385b39b00ae74b783cc"
+
+
+
